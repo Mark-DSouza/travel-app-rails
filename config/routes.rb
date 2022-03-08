e0fed5b9eaf3eabe3dac8eddfb2ecb7d5238d2e5
Mark-DSouza <markdsouza434@gmail.com>
2022-03-09 01:02:52 +0530

    add destination controller and routes for destinations including show, edit, update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  # tag controller routes
   get "tags" => "tags#index"
   get "tags/:id" => "tags#show", as: :tag
+
+  # destination controller routes
+  get "destinations/:id" => "destinations#show", as: :destination
+  get "destinations/:id/edit" => "destinations#edit", as: :edit_destination
+  patch "destinations/:id" => "destinations#update"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
