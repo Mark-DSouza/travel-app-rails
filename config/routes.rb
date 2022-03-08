d6a99ee6b5ed89b0b9a544b74dbade78ef8a661f
Mark-DSouza <markdsouza434@gmail.com>
2022-03-08 23:58:05 +0530

    add tag controller and routes for tag including index, show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "tags" => "tags#index"
+  get "tags/:id" => "tags#show", as: :tag
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
